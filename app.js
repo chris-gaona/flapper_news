a947bae21144c3f169d3862231efddfc2ccbc3bd
Chris Gaona <thebeloved88@sbcglobal.net>
2016-03-30 18:16:48 -0700

    Adds twitter bootstrap to add some styles easily

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('mainCtrl', ['$scope', function($scope) {
+app.factory('posts', [function() {
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
+app.controller('mainCtrl', ['$scope', 'posts', function($scope, posts) {
   $scope.test = 'Hello World!';
 
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+  $scope.posts = posts.posts;
+
+  // $scope.posts = [
+  //   {title: 'post 1', upvotes: 5},
+  //   {title: 'post 2', upvotes: 2},
+  //   {title: 'post 3', upvotes: 15},
+  //   {title: 'post 4', upvotes: 9},
+  //   {title: 'post 5', upvotes: 4}
+  // ];
 
   $scope.addPost = function() {
     if (!$scope.title || $scope.title === '') {
