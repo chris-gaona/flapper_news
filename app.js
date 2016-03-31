d38b7dee50b0c30535f07768bcc130218595ca85
Chris Gaona <thebeloved88@sbcglobal.net>
2016-03-30 17:50:15 -0700

    Adds functionality for user to enter post in input field

--- app.js
+++ app.js
@@ -9,5 +9,13 @@ app.controller('mainCtrl', ['$scope', function($scope) {
     {title: 'post 3', upvotes: 15},
     {title: 'post 4', upvotes: 9},
     {title: 'post 5', upvotes: 4}
-  ]
+  ];
+
+  $scope.addPost = function() {
+    if (!$scope.title || $scope.title === '') {
+      return;
+    }
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = '';
+  };
 }]);
